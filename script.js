4615508efaec935734e107223e96c13de934cb6f
joaoroccella <roccella@gmail.com>
2024-05-22 15:50:06 -0300

    fix: tratamento de erros

--- script.js
+++ script.js
@@ -2,19 +2,67 @@ function consultaCEP(cep) {
 
     cep = cep.replace(/\D/g, '');  // expressão regular -> regex
 
-    const requisicao = new Request(`https://viacep.com.br/ws/${cep}/json`, {
-        "method": "GET",
-        "headers": {
-            "Content-type": "application/json"
-        }
-    });
+    if (cep != "") {
+
+        const padraoCep = /^[0-9]{8}$/; // regex
+
+        if (padraoCep.test(cep)) {
+
+            document.querySelector('#bairro').setAttribute('readonly', '');
+            document.querySelector('#cidade').setAttribute('readonly', '');
+            document.querySelector('#uf').setAttribute('readonly', '');
+
+            const requisicao = new Request(`https://viacep.com.br/ws/${cep}/json`, {
+                "method": "GET",
+                "headers": {
+                    "Content-type": "application/json"
+                }
+            });
     
-    fetch(requisicao)
-        .then(resposta => resposta.json())
-        .then(resposta => {
-            document.querySelector('#logradouro').value = resposta.logradouro;
-            document.querySelector('#bairro').value = resposta.bairro;
-            document.querySelector('#cidade').value = resposta.localidade;
-            document.querySelector('#uf').value = resposta.uf;
-        });
+            fetch(requisicao)
+                .then(resposta => resposta.json())
+                .then(resposta => {
+
+                    if (!("erro" in resposta)) {
+
+                        document.querySelector('#logradouro').value = resposta.logradouro;
+                        document.querySelector('#bairro').value = resposta.bairro;
+                        document.querySelector('#cidade').value = resposta.localidade;
+                        document.querySelector('#uf').value = resposta.uf;
+                    
+                    } else {
+
+                        limpaForm();
+                        window.alert('CEP não localizado');
+
+                        document.querySelector('#bairro').removeAttribute('readonly');
+                        document.querySelector('#cidade').removeAttribute('readonly');
+                        document.querySelector('#uf').removeAttribute('readonly');
+
+                        document.querySelector('#logradouro').focus();
+
+                    }    
+                });
+
+        } else { // caso o cep esteja fora do padrão
+
+            limpaForm();
+            window.alert('O formato do CEP é inválido');
+
+        }
+
+    } else { // caso o cep esteja vazio
+
+        limpaForm();
+        window.alert('Digite um CEP!');
+    }
+
+} // fecha a função
+
+function limpaForm() {
+    
+    document.querySelectorAll('input:not(#cep)').forEach(input => {
+        input.value = '';
+    })
+
 }
